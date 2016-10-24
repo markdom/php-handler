4c8634dd68e1fbfbda77d2e857df171b19383aa6
Martin Brecht-Precht <mb@markenwerk.net>
2016-10-24 11:01:28 +0200

    Added YAML handler

---
+++ example/yaml.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Markdom\Test;
+
+use Markdom\Dispatcher\JsonDispatcher;
+use Markdom\Handler\YamlHandler;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$handler = new YamlHandler();
+$handler
+	->setPrettyPrint(false)
+	->setWordWrap(false);
+$dispatcher = new JsonDispatcher(file_get_contents(__DIR__ . '/example-data.json'));
+$dispatcher->dispatchTo($handler);
+fwrite(STDOUT, $handler->getResult());
---
+++ src/Handler/YamlHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Markdom\Handler;
+
+/**
+ * Class YamlHandler
+ *
+ * @package Markdom\Handler
+ */
+class YamlHandler extends PhpObjectHandler
+{
+
+	/**
+	 * @var bool
+	 */
+	private $prettyPrint = false;
+
+	/**
+	 * @var bool
+	 */
+	private $wordWrap = false;
+
+	/**
+	 * @return bool
+	 */
+	public function getPrettyPrint()
+	{
+		return $this->prettyPrint;
+	}
+
+	/**
+	 * @param bool $prettyPrint
+	 * @return $this
+	 */
+	public function setPrettyPrint($prettyPrint)
+	{
+		$this->prettyPrint = $prettyPrint;
+		return $this;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function getWordWrap()
+	{
+		return $this->wordWrap;
+	}
+
+	/**
+	 * @param boolean $wordWrap
+	 * @return $this
+	 */
+	public function setWordWrap($wordWrap)
+	{
+		$this->wordWrap = $wordWrap;
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getResult()
+	{
+		$indent = ($this->prettyPrint !== false) ? 4 : false;
+		/** @noinspection PhpUndefinedClassInspection */
+		$yaml = new \Spyc();
+		/** @noinspection PhpParamsInspection */
+		return $yaml->YAMLDump(parent::getResult(), $indent, $this->getWordWrap());
+	}
+
+}
